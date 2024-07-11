5c7afad7b7c96dbe4e5c76393ca0d9995f16a6a2
Usama Adil <usama.adil@phaedrasolutions.com>
2024-07-11 19:51:11 +0500

    -working on payment methods

--- src/app/(customer)/page.tsx
+++ src/app/(customer)/page.tsx
@@ -1,6 +1,7 @@
 import { ProductCard, ProductCardSkeleton } from "@/components/ProductCard"
 import { Button } from "@/components/ui/button"
 import db from "@/db/db"
+import { cache } from "@/lib/cache"
 import { Product } from "@prisma/client"
 import { ArrowRight } from "lucide-react"
 import Link from "next/link"
@@ -11,20 +12,21 @@ type ProductGridSectionProps = {
     productsFetcher: () => Promise<Product[]>;
 }
 
-function getMostPopularProducts() {
+const getMostPopularProducts = cache(() => {
     return db.product.findMany({
         where: { isAvailableForPurchase: true },
         orderBy: { orders: { _count: "desc" } },
         take: 6
     })
-}
-function getNewestProducts() {
+}, ["/", "getMostPopularProducts"], { revalidate: 60 * 60 * 24 })
+
+const getNewestProducts = cache(() => {
     return db.product.findMany({
         where: { isAvailableForPurchase: true },
         orderBy: { createdAt: "desc" },
         take: 6
     })
-}
+}, ["/", "getNewestProducts"])
 export default function HomePage() {
     return (
         <main className="space-y-12">
--- src/app/admin/_actions/products.ts
+++ src/app/admin/_actions/products.ts
@@ -4,6 +4,7 @@ import db from "@/db/db";
 import { z } from "zod";
 import fs from "fs/promises"
 import { notFound, redirect } from "next/navigation";
+import { revalidatePath } from "next/cache";
 
 const fileSchema = z.instanceof(File, { message: "Required" })
 const imageSchema = fileSchema.refine(
@@ -46,11 +47,15 @@ export async function addProduct(prevState: unknown, formData: FormData) {
             imagePath
         }
     });
+    revalidatePath("/");
+    revalidatePath("/products");
     redirect("/admin/products");
 }
 
 export async function toggleProductAvailability(id: string, isAvailableForPurchase: boolean) {
     await db.product.update({ where: { id }, data: { isAvailableForPurchase } })
+    revalidatePath("/");
+    revalidatePath("/products");
 }
 
 export async function deleteProduct(id: string) {
@@ -58,6 +63,8 @@ export async function deleteProduct(id: string) {
     if (product == null) return notFound();
     await fs.unlink(product.filePath);
     await fs.unlink(`public${product.imagePath}`)
+    revalidatePath("/");
+    revalidatePath("/products");
 }
 
 
@@ -95,5 +102,7 @@ export async function updateProduct(id: string, prevState: unknown, formData: Fo
             imagePath
         }
     });
+    revalidatePath("/");
+    revalidatePath("/products");
     redirect("/admin/products");
 }
